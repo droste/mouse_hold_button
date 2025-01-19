c035f9187fdd30cbd4857e7856cb5a241782e55c
droste <iot@pkt-inc.de>
2025-01-19 20:34:33 +0100

    Added functionality

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from pynput import mouse
-from mouse.mouse import on_click
-
-
-if __name__ == "__main__":
-    with mouse.Listener(
-        on_click=on_click
-    ) as listener:
-        listener.join()
